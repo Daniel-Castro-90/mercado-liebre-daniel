2cc41da12cef0d94c88386a05909aeecffbb0def
Daniel <castrodanieladr@gmail.com>
2023-09-19 20:04:33 -0300

    listo

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+//Puerto configurado bajo variable de entorno.
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+// Configura express para archivos desde /public
+app.use(express.static('public'));
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor iniciado correctamente en el puerto ${port}`);
+});
