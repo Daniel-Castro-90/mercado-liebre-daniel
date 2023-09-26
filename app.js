c2881d5337aa51bde49eeca20f7a30cdaf97ec5a
Daniel <castrodanieladr@gmail.com>
2023-09-25 22:12:01 -0300

    finalización de M04C21

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/login.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+
 // Configura express para archivos desde /public
 app.use(express.static('public'));
 
